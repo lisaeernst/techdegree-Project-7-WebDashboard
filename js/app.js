78f682ed6b8456fb58f06578c298796370afa9df
lisaeernst <78939274+lisaeernst@users.noreply.github.com>
2021-05-20 15:50:17 -0700

    css for tab

--- js/app.js
+++ js/app.js
@@ -10,11 +10,11 @@ const trafficCanvas = document.getElementById("trafficChart");
 alertBanner.innerHTML =
 `
 <div class="alert-banner">
-<p><strong>Alert:</strong> You have <strong>6</strong> overdue tasks
+<p class="alertText"><strong>Alert:</strong> You have <strong>6</strong> overdue tasks
 to complete</p>
 <p class="alert-banner-close">x</p>
 </div> `
-
+;
 alertBanner.addEventListener('click', e => {
     const element = e.target;
     if (element.classList.contains("alert-banner-close")) {
