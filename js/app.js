2b0c72eba8b588f14740ecf4520cba5d90b8feb2
lisaeernst <78939274+lisaeernst@users.noreply.github.com>
2021-05-25 12:50:15 -0700

    user search functioning

--- js/app.js
+++ js/app.js
@@ -22,14 +22,14 @@ alertBanner.addEventListener('click', e => {
 const trafficCanvas = document.getElementById("trafficChart");
 
 let trafficData = {
-    labels: ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3",
-    "4-10", "11-17", "18-24", "25-31"], 
-    datasets: [{ 
-        data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500,
-    2500],
-    backgroundColor: 'rgba(116, 119, 191, .3)',
+    labels: ["12-1", "1-2", "3-4", "5-6", "7-8", "9-10", "11-12"], 
+    datasets: [
+      { 
+        data: [750, 1450, 1500, 2200, 1800, 950, 1350],
+          backgroundColor: 'rgba(116, 119, 191, .3)',
           borderWidth: 1,
-        }]
+        }
+      ]
     };
 
     let trafficOptions = { 
@@ -126,7 +126,7 @@ const mobileData = {
             options: mobileOptions
         });
         
-        const user = document.getElementById("userField"); 
+        const user = document.getElementById("userSearch"); 
         const message = document.getElementById("messageField"); 
         const send = document.getElementById("send"); 
 
@@ -140,6 +140,7 @@ const mobileData = {
             alert("Please fill out message field before sending");
           } else {
             alert(`Message successfully sent to: ${user.value}`);
+            
           }
         });
 
