c86737f8a68405c278953e7df49903de518db442
lisaeernst <78939274+lisaeernst@users.noreply.github.com>
2021-05-25 19:55:44 -0700

    social media icons fixed border radius issues

--- js/app.js
+++ js/app.js
@@ -31,8 +31,10 @@ let trafficData = {
     datasets: [
       { 
         data: [750, 1450, 1500, 2200, 1800, 950, 1350],
-          backgroundColor: 'rgba(116, 119, 191, .3)',
-          borderWidth: 1,
+          backgroundColor: 'rgb(116 119 191)',
+          borderWidth: 2,
+          borderColor: 'rgb(77 76 114)',
+         
         }
       ]
     };
@@ -79,6 +81,26 @@ dailyL.addEventListener('click', function(event)  {
   monthlyL.classList.remove('active');
   trafficChart.data.datasets[0].data = [1500, 1250, 1354, 1339, 1830, 1452, 1820];
   trafficChart.data.labels = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+  trafficChart.update();
+});
+weeklyL.addEventListener('click', function(event)  {
+  event.preventDefault();
+  dailyL.classList.remove('active');
+  hourlyL.classList.remove('active');
+  weeklyL.classList.add('active');
+  monthlyL.classList.remove('active');
+  trafficChart.data.datasets[0].data = [820, 1850, 1654, 1439, 2130, 2052, 1820];
+  trafficChart.data.labels = ['Week 1', 'Week 2', 'Week 3', 'Week 4', 'Week 5', 'Week 6', 'Week 7'];
+  trafficChart.update();
+});
+monthlyL.addEventListener('click', function(event)  {
+  event.preventDefault();
+  dailyL.classList.remove('active');
+  hourlyL.classList.remove('active');
+  weeklyL.classList.remove('active');
+  monthlyL.classList.add('active');
+  trafficChart.data.datasets[0].data = [820, 1250, 1554, 1439, 1530, 1650, 1820, 1620, 1200, 975, 1049, 1120];
+  trafficChart.data.labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
   trafficChart.update();
 });
 
